14d1524ff2e06e8744ce248e08917b91a40bef18
Marcel Reinhardt <marcel.reinhardt@seibert.group>
2024-10-04 00:15:23 +0200

    Add openldap-server with some base data to test django implementation

---
+++ tests/openldap-server/init-ldap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clean up the data directory
+# rm -rf /var/lib/ldap/*
+
+# Apply the base LDIF file using slapadd
+slapadd -f /etc/ldap/slapd.conf -l /ldifs/base.ldif
+
+# Fix permissions
+chown -R openldap:openldap /var/lib/ldap
+
+# Start the LDAP server
+slapd -d 256 -f /etc/ldap/slapd.conf -u openldap -g openldap
