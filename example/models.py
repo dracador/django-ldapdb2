201d983e8c584c1d7eb2b9d07a1f7b089773d487
Marcel Reinhardt <mreinhardt@seibert-media.net>
2024-10-13 19:32:49 +0200

    Fix models base_dn

--- example/models.py
+++ example/models.py
@@ -3,9 +3,9 @@ from ldapdb.models import LDAPModel
 
 
 class LDAPUser(LDAPModel):
-    base_dn = 'ou=People,dc=smhss,dc=de'
+    base_dn = 'ou=Users,dc=example,dc=org'
     object_classes = ['inetOrgPerson', 'organizationalPerson']
-    object_classes_minimal = ['inetOrgPerson']
+    object_classes_minimal = ['inetOrgPerson']  # These are the values used to search for existing entries
 
     uid = CharField(db_column='uid', primary_key=True)
 
