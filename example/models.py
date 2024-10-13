653dc2bbc9495cdc90294b714c4e0dca98cca249
Marcel Reinhardt <mreinhardt@seibert-media.net>
2024-10-13 22:58:13 +0200

    add name field to example, do not order for now

--- example/models.py
+++ example/models.py
@@ -7,8 +7,9 @@ class LDAPUser(LDAPModel):
     object_classes = ['inetOrgPerson', 'organizationalPerson']
     object_classes_minimal = ['inetOrgPerson']  # These are the values used to search for existing entries
 
-    uid = CharField(db_column='uid', primary_key=True)
+    username = CharField(db_column='uid', primary_key=True)
+    name = CharField(db_column='cn')
 
     class Meta:
         managed = False
-        ordering = ('uid',)
+        # ordering = ('uid',)
