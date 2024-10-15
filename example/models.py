feffd5121c65437bee16d272af12b9a43764696c
Marcel Reinhardt <mreinhardt@seibert-media.net>
2024-10-16 00:40:02 +0200

    set base_filter & ordering in example LDAPUser

--- example/models.py
+++ example/models.py
@@ -4,12 +4,12 @@ from ldapdb.models import LDAPModel
 
 class LDAPUser(LDAPModel):
     base_dn = 'ou=Users,dc=example,dc=org'
+    base_filter = '(objectClass=inetOrgPerson)'  # the base filter will be always be applied to all queries
     object_classes = ['inetOrgPerson', 'organizationalPerson']
-    object_classes_minimal = ['inetOrgPerson']  # These are the values used to search for existing entries
 
     username = CharField(db_column='uid', primary_key=True)
     name = CharField(db_column='cn')
 
     class Meta:
         managed = False
-        # ordering = ('uid',)
+        ordering = ('username',)
