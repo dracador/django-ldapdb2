f50df5ba2136851c1715c253bc4a41653a095317
Marcel Reinhardt <mreinhardt@seibert-media.net>
2024-10-28 01:37:03 +0100

    Remove obsolete blank & null attributes for LDAPUser, add non_existing_attribute field

--- example/models.py
+++ example/models.py
@@ -4,15 +4,16 @@ from ldapdb.models import LDAPModel
 
 class LDAPUser(LDAPModel):
     base_dn = 'ou=Users,dc=example,dc=org'
-    base_filter = '(objectClass=inetOrgPerson)'  # the base filter will be always be applied to all queries
+    base_filter = '(objectClass=inetOrgPerson)'
     object_classes = ['inetOrgPerson', 'organizationalPerson']
 
     username = CharField(db_column='uid', primary_key=True)
     name = CharField(db_column='cn')
-    first_name = CharField(db_column='givenName', blank=True, null=True)
+    first_name = CharField(db_column='givenName')
     last_name = CharField(db_column='sn')
-    mail = CharField(db_column='mail', blank=True, null=True)
+    mail = CharField(db_column='mail')
+    non_existing_attribute = CharField(db_column='nonExistingAttribute')
 
     class Meta:
         managed = False
-        ordering = ('username',)
+        ordering = ('username',)  # default ordering for SSSVLV is the primary_key. Without SSSVLV, no ordering occurs.
