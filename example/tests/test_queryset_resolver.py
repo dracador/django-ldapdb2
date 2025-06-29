f19b3879a1e9a22859996564f0d2bdba0bfe9a90
Marcel Reinhardt <marcel.reinhardt@seibert.group>
2025-06-29 05:18:14 +0200

    Add test cases for annotations

--- example/tests/test_queryset_resolver.py
+++ example/tests/test_queryset_resolver.py
@@ -1,3 +1,6 @@
+from django.db.models import Value
+from django.db.models.functions import Length, Lower
+
 from example.models import LDAPGroup, LDAPUser
 from .base import LDAPTestCase, get_new_ldap_search
 from .constants import TEST_LDAP_ADMIN_USER_1, TEST_LDAP_AVAILABLE_USERS, TEST_LDAP_GROUP_1, TEST_LDAP_USER_1
@@ -182,3 +185,26 @@ class QueryResolverTestCase(LDAPTestCase):
         obj.name = 'New name'  # Note: Setting the primary key to something else will result in a DoesNotExist error
         obj.refresh_from_db()
         self.assertLDAPModelObjectsAreEqual(TEST_LDAP_USER_1, obj)
+
+    # ---- Annotations ----
+    def test_annotation_constant(self):
+        qs = LDAPUser.objects.annotate(one=Value(1))
+        assert qs.first().one == 1
+
+    def test_annotation_expression(self):
+        qs = LDAPUser.objects.annotate(lower=Lower("username")).order_by("lower")
+        usernames = list(qs.values_list("lower", flat=True))
+        assert usernames == sorted(u.lower() for u in usernames)
+
+    def test_annotation_length_and_filter(self):
+        qs = (
+            LDAPUser.objects
+            .annotate(n=Length("username"))
+            .filter(n__gt=3)
+            .values_list("username", flat=True)
+        )
+        assert all(len(u) > 3 for u in qs)
+
+    def test_slice_with_annotation(self):
+        obj = LDAPUser.objects.annotate(lower=Lower("username")).order_by("lower")[1]
+        assert obj.username == TEST_LDAP_USER_1.username
