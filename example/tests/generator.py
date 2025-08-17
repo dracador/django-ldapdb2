53b0fa9514e81f62daf1069f9b7452123c28b61d
Marcel Reinhardt <mreinhardt@seibert-media.net>
2025-08-17 13:34:20 +0200

    Fix handling of field defaults and overall member attribute handling

--- example/tests/generator.py
+++ example/tests/generator.py
@@ -2,7 +2,12 @@ from random import choice
 
 from faker import Faker
 
-from example.models import LDAPUser
+from example.models import LDAPGroup, LDAPUser
+
+
+def generate_random_group_name() -> str:
+    fake = Faker()
+    return f'{fake.name()}-{choice(range(1000))}'
 
 
 def generate_random_username() -> str:
@@ -12,7 +17,24 @@ def generate_random_username() -> str:
     return f'{first_name.lower()}.{last_name.lower()}-{choice(range(1000))}'
 
 
-def create_random_ldap_user(do_not_create: bool = False, **kwargs) -> LDAPUser:
+def create_random_ldap_group(model_cls=LDAPGroup, do_not_create: bool = False, **kwargs) -> LDAPGroup:
+    name = generate_random_group_name()
+    default_kwargs = {
+        # dn=f'cn={name},ou=Groups,dc=example,dc=org',
+        'org_unit': 'group1',
+        'name': name,
+        'members': [
+            'uid=admin,ou=Users,dc=example,dc=org',
+            'uid=user1,ou=Users,dc=example,dc=org',
+        ],
+    }
+    if do_not_create:
+        return model_cls(**{**default_kwargs, **kwargs})
+
+    return model_cls.objects.create(**{**default_kwargs, **kwargs})
+
+
+def create_random_ldap_user(model_cls=LDAPUser, do_not_create: bool = False, **kwargs) -> LDAPUser:
     fake = Faker()
     first_name = fake.first_name()
     last_name = fake.last_name()
@@ -27,6 +49,6 @@ def create_random_ldap_user(do_not_create: bool = False, **kwargs) -> LDAPUser:
         'is_active': True,
     }
     if do_not_create:
-        return LDAPUser(**{**default_kwargs, **kwargs})
+        return model_cls(**{**default_kwargs, **kwargs})
 
-    return LDAPUser.objects.create(**{**default_kwargs, **kwargs})
+    return model_cls.objects.create(**{**default_kwargs, **kwargs})
--- ldapdb/validators.py
+++ ldapdb/validators.py
@@ -2,6 +2,6 @@ import ldap
 from django.core.exceptions import ValidationError
 
 
-def validate_dn(value):
+def validate_dn(value: str):
     if not ldap.dn.is_dn(value):
         raise ValidationError(f'Invalid distinguished name: {value}')
